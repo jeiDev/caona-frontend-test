0d3bb0a2a1640d27665a9a8600e910ef9dbb9496
JeiDevp <joseanyeloserrano@gmail.com>
2024-02-04 12:00:23 -0400

    fixed bug menu responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
       <head>
         <link rel="icon" type="image/png" href="/images/favicon.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
       </head>
       <body>
         <Provider store={store}>
